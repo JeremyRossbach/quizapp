90e2d04cd6c4dff80561f03d2c1b00348322f95e
Jeremy Roßbach <jeremy1899@web.de>
2024-02-29 14:40:18 +0100

    finished quizapp

--- script.js
+++ script.js
@@ -42,7 +42,16 @@ let questions = [
 ];
 
 
+let rightAnswers = 0;
 let currentQuestion = 0;
+let AUDIO_RIGHT = new Audio('audio/right.mp3');
+let AUDIO_WRONG = new Audio('audio/wrong.mp3');
+
+
+function startQuiz() {
+    document.getElementById('beginningscreen').style = 'display: none';
+    document.getElementById('question-body').style = 'flex';
+}
 
 
 function init() {
@@ -53,10 +62,42 @@ function init() {
 
 
 function showQuestion() {
+    if (gameIsOver()) {
+        showEndscreen();
+    } else {
+        updateProgressbar();
+        updateToNextQuestion();
+    }
+}
+
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+
+function showEndscreen() {
+    document.getElementById('endscreen').style = '';
+    document.getElementById('question-body').style = 'display: none';
+
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amoutn-of-right-answers').innerHTML = rightAnswers;
+}
+
+
+function updateProgressbar() {
+    let percent = (currentQuestion) / questions.length;
+    percent = Math.round(percent * 100);
+
+    document.getElementById('progress-bar').style = `width: ${percent}%`;
+}
+
+
+function updateToNextQuestion() {
     let question = questions[currentQuestion];
 
+    document.getElementById('number-current-question').innerHTML = currentQuestion + 1;
     document.getElementById('question-text').innerHTML = question['question'];
-
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
@@ -66,14 +107,48 @@ function showQuestion() {
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    console.log('Selected answer is', selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQuestionNumber is', selectedQuestionNumber);
-    console.log('Current question is', question['right_answer']);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question['right_answer']) {
+    if (rightAnswerSelected(selectedQuestionNumber)) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightAnswers++;
+        AUDIO_RIGHT.play();
     } else {
-        document.getElementById(selection).parentNode.classList.add('bg-danger');    
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_WRONG.play();
     }
+    document.getElementById('next-button').disabled = false;
+}
+
+
+function rightAnswerSelected(selectedQuestionNumber) {
+    let question = questions[currentQuestion];
+
+    return selectedQuestionNumber == question['right_answer'];
+}
+
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswers();
+    showQuestion();
+}
+
+
+function resetAnswers() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function startAgain() {
+    location.reload();
 }
